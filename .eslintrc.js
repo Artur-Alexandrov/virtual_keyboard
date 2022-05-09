b4bb08fded930a44150d37b328c9740baee94985
Artur-Alexandrov <arturches@gmail.com>
2022-05-09 21:12:47 +0300

    fix: set up eslint and fix some esling problems

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+
+  },
+
+  extends: [
+    'airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': 0,
+    'linebreak-style': 0,
+    'max-len': 'off',
+  },
+};
